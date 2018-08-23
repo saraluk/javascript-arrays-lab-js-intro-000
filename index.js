b90c0498a29a99d754e111808a7c5fef072d568b
saraluk <saralukkai@gmail.com>
2018-08-23 23:54:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,11 +31,11 @@ function prependKitten(name){
 }
 
 function removeLastKitten(){
-  kittens.slice(0,kittens.lenght-1);
+  kittens = kittens.slice(0,kittens.lenght-1);
   return kittens
 }
 
 function removeFirstKitten() {
-  kittens.slice(0,1);
+  kittens = kittens.slice(1);
   return kittens
 }
