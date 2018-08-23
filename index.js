a28a9b7a22e9e9bbaff81560a380ce6047b0c7e9
saraluk <saralukkai@gmail.com>
2018-08-23 23:49:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Add your functions and code here
+let kittens = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+  return kittens
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(name){
+  kittens.pop(name);
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(name){
+  kittens.shift(name);
+  return kittens
+}
+
+function appendKitten(name){
+  var cats = [...kittens,name];
+  return cats
+}
+
+function prependKitten(name){
+  var cats = [name,...kittens];
+  return cats
+}
+
+function removeLastKitten(){
+  var cats = kittens.slice(0,kittens.lenght-1);
+  return cats
+}
